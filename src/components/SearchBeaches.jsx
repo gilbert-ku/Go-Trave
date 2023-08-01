e58e4c480f37f712f0af57c7b59ec0791753d0c0
judy-chelangat <judysigilai75@gmail.com>
2023-08-01 18:54:40 +0300

    added comments

--- src/components/SearchBeaches.jsx
+++ src/components/SearchBeaches.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function SearchBeaches({ beaches = [] }) {
   const [searchTerm, setSearchTerm] = useState(""); // State to hold the user's input
 
+  //filtering the beaches based on the search term 
   const filteredBeaches = beaches.filter((location) => {
     const beachLocation = location.location || ""; // Provide a default value if location is missing
     return beachLocation.toLowerCase().includes(searchTerm.toLowerCase());
@@ -22,6 +23,7 @@ function SearchBeaches({ beaches = [] }) {
       </div>
 
       <div className='main-container'>
+        {/* conditional rendering to display the beaches based on the users' search input */}
         {searchTerm !== ""
           ? filteredBeaches.map((item) => (
               <div key={item.id} className='beach-item'>
