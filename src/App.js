7c6b5a9d5089e44e2821bb6dc8afa5bb1bf8f670
judy-chelangat <judysigilai75@gmail.com>
2023-08-01 11:41:33 +0300

    rendered beaches component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-
 import './App.css';
 
-import Hotels from "./components/Hotels";
-import AddHotelForm from './components/AddHotelForm';
 function App() {
-    return(
-        <div>
-           <AddHotelForm />
-            <Hotels />
-        </div>
-        
-    )
+
 
 }
 
 export default App;
-
