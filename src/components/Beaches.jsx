3664a4a3df8144805269bd3753e8d85275fa4f49
judy-chelangat <judysigilai75@gmail.com>
2023-08-01 11:43:16 +0300

    fetched the data and used map to display the items

--- src/components/Beaches.jsx
+++ src/components/Beaches.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+
 
 function Beaches() {
+    const[data,setData]=useState([])
+
+    useEffect(()=>{
+        fetch("https://travel-ke.onrender.com/hotels")
+        .then(res=>res.json())
+        .then(data=>{
+            setData(data)
+            console.log(data)
+        })
+    },[])
+
+    const displayBeaches=data.map((item)=>{
+      return(
+        <div>
+          <img src={item.image_url}/>
+          <div>
+            <h4>{}</h4>
+          </div>
+        </div>
+      )
+
+    })
   return (
-    <div>
-      
-    </div>
+    <>
+      <displayBeaches/>
+    </>
   )
 }
 
-export default Beaches
+export default Beaches;
