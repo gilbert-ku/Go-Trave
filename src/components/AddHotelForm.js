abc836aa10b8f8a1c31f492dc5eceba9b3683725
gilbert-ku <gilbert45ku@gmail.com>
2023-08-02 14:20:57 +0300

    get form value anf use asyn and wait to post data

--- src/components/AddHotelForm.js
+++ src/components/AddHotelForm.js
@@ -1,18 +1,96 @@
 
+import React, { useState } from "react";
+
 
 function AddHotelForm() {
-
+    const [image, setImage] = useState("");
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [rating, setRating] = useState("");
+    const [amenities, setAmenities] = useState("");
+    const [hotelsList, setHotelsList] = useState([]);
+    function handleInput(event) {
+        const { name, value } = event.target;
+        switch (name) {
+          case "image":
+            setImage(value);
+            break;
+          case "name":
+            setName(value);
+            break;
+          case "address":
+            setAddress(value);
+            break;
+          case "description":
+            setDescription(value);
+            break;
+          case "price":
+            setPrice(value);
+            break;
+          case "rating":
+            setRating(value);
+            break;
+          case "amenities":
+            setAmenities(value.split(","));
+            break;
+          default:
+            break;
+        }
+      }
+    
+    // state to tracking the id of the new hotel
+    const [newHotelId, setNewHotelId] = useState(1);
+  
+    async function handleSubmit(e) {
+      e.preventDefault();
+      const formData = {
+        image,
+        name,
+        address,
+        description,
+        price,
+        rating,
+        amenities,
+      };
+  
+      try {
+        const response = await fetch("http://localhost:8001/hotels", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+  
+        if (!response.ok) {
+          throw new Error("Failed to add hotel");
+        }
+  
+        const data = await response.json();
+        console.log("form data", data);
+  
+        // Add the new hotel to the hotelsList with an id
+        const newHotel = { ...data, id: newHotelId };
+        setHotelsList([...hotelsList, newHotel]);
+        setNewHotelId(newHotelId + 1); // Increment the id for the next hotel
+      } catch (error) {
+        console.error("Error sending data", error);
+      }
+    }
   return (
     <div >
       <h1>Add hotel</h1>
-      <form className="AddHotelForm" >
+      <form className="AddHotelForm" onSubmit={handleSubmit}>
         <label>Hotel Image</label>
         <br />
         <input
           type="url"
           name="image"
           placeholder="Hotel image"
-
+          value={image}
+          onChange={handleInput}
         />
         <br />
         <br />
@@ -22,7 +100,8 @@ function AddHotelForm() {
           type="text"
           name="name"
           placeholder="Hotel name"
-
+          value={name}
+          onChange={handleInput}
         />
         <br />
         <br />
@@ -32,7 +111,8 @@ function AddHotelForm() {
           type="text"
           name="address"
           placeholder="Address"
-
+          value={address}
+          onChange={handleInput}
         />
         <br />
         <br />
@@ -42,7 +122,8 @@ function AddHotelForm() {
           type="text"
           name="description"
           placeholder="Description"
-
+          value={description}
+          onChange={handleInput}
         />
         <br />
         <br />
@@ -52,7 +133,8 @@ function AddHotelForm() {
           type="number"
           name="price"
           placeholder="Price in ($)"
-
+          value={price}
+          onChange={handleInput}
         />
         <br />
         <br />
@@ -62,7 +144,8 @@ function AddHotelForm() {
           type="number"
           name="rating"
           placeholder="Rating"
-
+          value={rating}
+          onChange={handleInput}
         />
         <br />
         <br />
@@ -72,7 +155,8 @@ function AddHotelForm() {
           type="text"
           name="amenities"
           placeholder="Amenities"
-
+          value={amenities}
+          onChange={handleInput}
         />
         <br />
         <br />
@@ -80,8 +164,6 @@ function AddHotelForm() {
           Submit
         </button>
       </form>
-
-      <h2>Newly added hotels</h2>
     </div>
   );
 }
