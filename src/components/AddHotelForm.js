7c00fbcae6925d3dcfb78ec8ffc46d31fbd3eec5
gilbert-ku <gilbert45ku@gmail.com>
2023-08-02 14:13:08 +0300

    add form

--- src/components/AddHotelForm.js
+++ src/components/AddHotelForm.js
@@ -1,12 +1,89 @@
 
 
-
-
 function AddHotelForm() {
 
   return (
-    <h1>Added hotel</h1>
-  )
+    <div >
+      <h1>Add hotel</h1>
+      <form className="AddHotelForm" >
+        <label>Hotel Image</label>
+        <br />
+        <input
+          type="url"
+          name="image"
+          placeholder="Hotel image"
+
+        />
+        <br />
+        <br />
+        <label>Hotel Name</label>
+        <br />
+        <input
+          type="text"
+          name="name"
+          placeholder="Hotel name"
+
+        />
+        <br />
+        <br />
+        <label>Hotel Address</label>
+        <br />
+        <input
+          type="text"
+          name="address"
+          placeholder="Address"
+
+        />
+        <br />
+        <br />
+        <label>Hotel Description</label>
+        <br />
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+
+        />
+        <br />
+        <br />
+        <label>Price in ($)</label>
+        <br />
+        <input
+          type="number"
+          name="price"
+          placeholder="Price in ($)"
+
+        />
+        <br />
+        <br />
+        <label>Hotel Rating</label>
+        <br />
+        <input
+          type="number"
+          name="rating"
+          placeholder="Rating"
+
+        />
+        <br />
+        <br />
+        <label>Hotel Amenities</label>
+        <br />
+        <input
+          type="text"
+          name="amenities"
+          placeholder="Amenities"
+
+        />
+        <br />
+        <br />
+        <button type="submit" className="Submitbtn">
+          Submit
+        </button>
+      </form>
+
+      <h2>Newly added hotels</h2>
+    </div>
+  );
 }
 
 export default AddHotelForm;
