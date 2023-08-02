1eeb38bde733f01beef65edc8f914b7c78f8320e
gilbert-ku <gilbert45ku@gmail.com>
2023-08-02 13:44:10 +0300

    fetch hotel data

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,13 +1,21 @@
 // import "./Cards.css"
-
+import React, { useState, useEffect } from "react";
 
 function Hotels() {
- 
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    fetch(" http://localhost:8001/hotels")  
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data)
+        setHotels(data);
+      });
+  }, []);
 
   return (
     <>
     <h3>Hotels and their ratings</h3>
-
     </>
   );
 }
