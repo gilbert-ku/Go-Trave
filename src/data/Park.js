8095c4d5e8cd9543b50e13e36e3e0ae775045997
mitunda <peterbrian28@gmail.com>
2023-08-02 15:25:25 +0300

    created the data folder

---
+++ src/data/Park.js
@@ -0,0 +1,21 @@
+const parks = [
+    {
+        id: 'p1',
+        name: 'Yellowstone National Park', 
+        location: 'Wyoming',
+        amenities: ['hiking', 'camping', 'fishing']
+      },
+      {
+        id: 'p2', 
+        name: 'Yosemite National Park',
+        location: 'California',
+        amenities: ['hiking', 'camping']
+      },
+      {
+        id: 'p3',
+        name: 'Zion National Park',
+        location: 'Utah',
+        amenities: ['hiking', 'camping', 'canyoneering']
+      },
+      //... more parks
+];
