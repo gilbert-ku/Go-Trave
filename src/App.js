fc53ee0ade35ed6c23e2a0374d8bfac52d6f4924
gilbert-ku <gilbert45ku@gmail.com>
2023-08-03 20:07:56 +0300

    add changes to image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
+import React, { useState } from 'react';
 import Hotels from "./components/Hotels";
 import AddHotelForm from './components/AddHotelForm';
+import Beaches from "./components/Beaches";
+import Search from "./components/Search";
+import RanchList from "./components/RanchList";
+import BookingForm from "./components/BookingForm";
 
-    return(
-        <div>
-            <AddHotelForm />
-            <Hotels />
-            <Beaches />
-        </div>
-        
-    )
+function App() {
+  const [filteredRanches, setFilteredRanches] = useState([]);
+  const [ranches, setRanches] = useState([]); // You should have an initial state for ranches
 
+  const handleSearch = (filteredRanches) => {
+    console.log('Filtered ranches:', filteredRanches);
+    setFilteredRanches(filteredRanches);
+  };
 
+  const handleBooking = (bookingData) => {
+    // Handle booking logic here
+  };
 
-    
-
-
-<<<<<<< HEAD
-export default App;
-=======
   return (
     <div className='main-container'>
-      <Home />
       <AddHotelForm />
       <Hotels />
       <Beaches />
@@ -32,5 +32,4 @@ export default App;
   );
 }
 
-export default App
->>>>>>> b84149c (add merged work)
+export default App;
