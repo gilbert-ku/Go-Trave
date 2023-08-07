f81062c7b657364bfa6ab64a9a8d5b20dc7c559e
gilbert-ku <gilbert45ku@gmail.com>
2023-08-07 09:42:47 +0300

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
-import RanchList from './components/RanchList';
-import Search from './components/Search';
-import Home from './pages/Home';
-import Hotels from "./components/Hotels";
-import AddHotelForm from './components/AddHotelForm';
-import BookingForm from './components/BookingForm';
-import Beaches from './components/Beaches';
 
 function App() {
-  const [ranches, setRanches] = useState([]);
-  const [filteredRanches, setFilteredRanches] = useState([]);
 
-  useEffect(() => {
-    // Fetch the ranch data from the provided URL
-    fetch('http://localhost:8001/ranches')
-      .then((response) => response.json())
-      .then((data) => {
-        setRanches(data);
-      })
-      .catch((error) => console.error('Error fetching ranch data:', error));
-  }, []);
 
-  const handleBooking = (formData) => {
-    console.log('Booking ranch:', formData);
-    // Implement the booking logic here
-  };
-
-  const handleSearch = (filteredRanches) => {
-    console.log('Filtered ranches:', filteredRanches);
-    setFilteredRanches(filteredRanches);
-  };
-
-  return (
-    <div className='main-container'>
-      <Home />
-      <AddHotelForm />
-      <Hotels />
-      <Beaches />
-      <Search ranches={ranches} onSearch={handleSearch} />
-      <RanchList ranches={filteredRanches.length > 0 ? filteredRanches : ranches} />
-      <BookingForm onBooking={handleBooking} />
-    </div>
-  );
 }
 
 export default App;
